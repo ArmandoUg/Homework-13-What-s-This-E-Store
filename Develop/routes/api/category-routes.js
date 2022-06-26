dcbf1626e9238d2b083623969c55dc3fbe91ddc1
Armando Ugalde <uarmando15@yahoo.com>
2022-06-25 18:57:33 -0700

    began to add logic to api(ex get,post

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,18 +3,40 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try{
+    const allCategories = await Category.findAll({include: [{
+      model: Product}],
+    })
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json("An Error occured", err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+  const singleCategory= Category.findByPk(req.params.id, {
+    include: [{
+      model: Product
+    }],
+  })
+  res.status(200).json(singleCategory);
+} catch (err) {
+  res.status(500).json("An Error occurred", err);
+}
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const oneCategory = await Category.create(req.body);
+    res.status(200).json(oneCategory);
+  } catch(err) {
+    res.status(400).json(`An Error occurred`, err)
+  }
 });
 
 router.put('/:id', (req, res) => {
