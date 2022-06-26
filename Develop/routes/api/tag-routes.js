202a0b13b0b922a06dc23cdce9a0106910544b78
Armando Ugalde <uarmando15@yahoo.com>
2022-06-25 22:45:47 -0700

    added logic for get delete for tag route

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -13,24 +13,66 @@ try{
       })
       res.status(200).json(allTags);
     } catch (err) {
-      res.status(500).json("An Error occured", err);
-    };
+      res.status(500).json("An Error occurred", err);
+    }
+    });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const singleTag = Tag.findByPk(req.params.id, {
+      include:[{
+        model:Product}],
+        })
+        if(!singleTag){
+        req.status(404).json('We could not find that tag')
+      }
+      res.status(200).json(singleTag)
+  }
+  catch (err) {
+    res.status(500).json("An Error occurred", err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try{
+    const oneTag = Tag.create(req.body);
+    res.status(200).json(oneTag);
+  } catch (err) {
+    res.status(400).json(`An Error occurred`, err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const updatedTag = Tag.update(req.body, { 
+      where: { id: req.params.id } 
+    })
+    if (!updatedTag){
+      res.status(404).json('We could not find that tag')
+      return;
+    }
+    res.status(200).json(`Successfully updated tag ${req.params.id}`);
+  } catch (err) {
+    res.status(500).json(`An Error occurred`, err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const deletedTag = Tag.destroy({ where: { id: req.params.id } 
+    })
+    if (!deletedTag){
+      res.status(404).json('We could not find that tag')
+    }
+    res.status(200).json(`Successfully deleted tag ${req.params.id}`);
+  } catch (err) {
+    res.status(500).json(`An Error occurred`, err);
+  }
 });
 
 module.exports = router;
